944ce2bec617cdfeed28e6c3a9942920d48d5038
Abey Onalaja <Abey.onalaja@gmail.com>
2021-09-23 19:51:54 +0100

    Initial Commit

---
+++ DLogin/DLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLoginApp.swift
+//  DLogin
+//
+//  Created by Abideen Onalaja on 23/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct DLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
